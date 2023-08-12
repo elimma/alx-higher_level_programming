b0e7cbd056eca0cb29ce73076bb24c26aab30d24
Kourdan Oumaima <ishikawaa83@gmail.com>
2023-08-12 05:18:55 -0700

    new list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,5 +3,5 @@ def new_in_list(my_list, idx, element):
     if idx < 0 or idx > (len(my_list) - 1):
         return (my_list)
     n_list = [x for x in my_list]
-    n_list[copy] = element
-    return (copy)
+    n_list[idx] = element
+    return (n_list)
