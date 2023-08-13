1e5433550d96f612e232aa4c6b23fbd442757058
Kourdan Oumaima <ishikawaa83@gmail.com>
2023-08-13 14:01:41 -0700

    multiple returns

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    leng = len(sentence)
+    if leng == 0:
+        results = (0, Non)
+        return results
+    else:
+        res = (leng, sentence[0:1])
+        return res
