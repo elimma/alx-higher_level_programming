39d7ffc896eea7d1dc181148d03edf90715a7f93
elimma <ishikawwa83@gmail.com>
2023-10-10 12:35:59 -0700

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,11 +5,4 @@
 def read_file(filename=""):
     """Print the contents of a UTF8 text file to stdout."""
     with open(filename, encoding="utf-8") as f:
-        print(f.read(), end="")#!/usr/bin/python3
-        """Defines a text file-reading function."""
-
-
-        def read_file(filename=""):
-                """Print the contents of a UTF8 text file to stdout."""
-                    with open(filename, encoding="utf-8") as f:
-                                print(f.read(), end="")
+        print(f.read(), end="")
